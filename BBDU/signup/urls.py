5bb09f64269e943cfe6782d407a0b5df5b89179b
Gaurav Tiwari <gauravtsm@gmail.com>
2023-06-16 23:26:00 +0530

    index

--- BBDU/signup/urls.py
+++ BBDU/signup/urls.py
@@ -1,3 +1,4 @@
+from operator import index
 from django.contrib import admin
 from django.urls import path
 from signup.views import signup, success
@@ -5,6 +6,7 @@ from signup.views import signup, success, logout_view
 
 urlpatterns = [
     path('', signup, name='signup'),
+    path('index/', index, name='index'),
     path('success/', success, name='success'),
     path('logout/', success, name='logout'),
 ]
